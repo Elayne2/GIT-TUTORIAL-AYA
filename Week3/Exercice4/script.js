9246dac4159d6f8de733d1fdca560ab48a819456
elayne <ayachokri27@gmail.com>
2023-05-09 05:28:48 +0200

    first commit

---
+++ Week3/Exercice4/script.js
@@ -0,0 +1,18 @@
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction:The outcome is 55 because 34+21=55
+// Actual:55
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction:Te outcome is 23 because 2+21+23
+// Actual:23
+
+// C doesn't have a value since we didn't define it
+
+console.log(3 + 4 + '5');
+// 3 and 4 are numbers which is not the cas for '5' since it is a string so 3+4=7 and 7+'5'=75
