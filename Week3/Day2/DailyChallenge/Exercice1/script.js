c4e069a7028aff7756f2e3fee2416997f0e23039
elayne <ayachokri27@gmail.com>
2023-05-17 20:25:31 +0200

    first commit

---
+++ Week3/Day2/DailyChallenge/Exercice1/script.js
@@ -0,0 +1,13 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+ 
+fruits.shift();
+
+fruits.sort();
+
+fruits.push("Kiwi");
+
+fruits.splice(fruits.indexOf("Apples"), 1);
+
+fruits.reverse();
+
+console.log(fruits);
